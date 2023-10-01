3a361be5ea71fbe6506653dce244ef2361da463a
Sahilk2912 <64694370+Sahilk2912@users.noreply.github.com>
2023-10-02 01:31:38 +0530

    Software Updates

--- cope.cpp
+++ cope.cpp
@@ -4,6 +4,13 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     cout << "too much cock you're showing ra";
-
+    int var;
+    cin >> var;
+    vector<int> a;
+    for(auto &i:a)
+        cin >> i;
+    for(auto i:a)
+        cout << i<<" ";
+    cout << endl;
     return 0;
 }
